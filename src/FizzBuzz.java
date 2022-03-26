24a27c44eacb3fa32a8276708ec487ad42bebec5
AIRI.K <102028347+invuilly@users.noreply.github.com>
2022-03-26 20:55:47 +0900

    FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,19 +1,18 @@
 
 public class FizzBuzz {
-	public static void main(String[]argn) {
-		for(int i = 1; i <= 0; i ++) {
-			if (i % 3 == 0) {
-				System.out.println("Fizz");
-				
-			}else if(i % 5 == 0) {
-				System.out.println("Buzz");
-				
-			}else  if(i % 3 == 0 && i % 5 == 0) {
+	public static void main(String[]args) {
+		
+		for(int i = 1; i <= 100; i ++) {
+			
+			if(i % 15 == 0) {
 				System.out.println("FizzBuzz");
+			}else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			}else if (i % 5 == 0) {
+				System.out.println("Buzz");
 			}else {
 				System.out.println(i);
 			}
 		}
 	}
-
 }
